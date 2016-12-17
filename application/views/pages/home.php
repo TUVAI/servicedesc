02e5bdec679072511ad7d5554aea6e33ad697f5c
Vadim Tatarchuk <tatarchuk.v.b@gmail.com>
2016-12-17 21:03:35 +0500

    add codeIgniter3 framework

---
+++ application/views/pages/home.php
@@ -0,0 +1,5 @@
+<h1><?php echo $title ?> -- 1</h1>
+<p>dfasdf asdf asdfasd</p>
+<p>dfasdf asdf asdfasd</p>
+<p>dfasdf asdf asdfasd</p>
+<p>dfasdf asdf asdfasd</p>
---
+++ application/views/pages/pages2.php
@@ -0,0 +1,1 @@
+<p><i>asfda asf das</i></p>
